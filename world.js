3cd5c469de713a1ff7f84d3d561f27d51a26cb75
Jeante <55599296+Jeante@users.noreply.github.com>
2019-11-20 23:22:42 -0500

    Add files via upload

--- world.js
+++ world.js
@@ -16,4 +16,21 @@ xhr.onreadystatechange = function(){
 xhr.open("GET", "world.php?country="+inputVal);
 xhr.send();
 })
+
+
+document.getElementById("lookup2").addEventListener("click", function(){
+    
+    var inputVal = document.getElementById("country").value;
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function(){
+    if (this.readyState == 4 && this.status == 200){
+      
+      document.getElementById("result").innerHTML = xhr.responseText;
+
+    }
+  }
+xhr.open("GET", " world.php?country="+inputVal);
+xhr.send();
+})
 }
--- world.php
+++ world.php
@@ -4,15 +4,26 @@ $username = 'lab7_user';
 $password = 'my_password';
 $dbname = 'world';
 $country1 = filter_var($_GET['country'], FILTER_SANITIZE_STRING);
-
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country1%'");
-
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
-<ul>
+
+
+
+
+<table border="7">
+    <tr>
+    <th>Name</th>
+    <th>Continent</th>
+    <th>Independence</th>
+    <th>Head of State</th>
+  </tr>
+  
 <?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+
+  <tr><td><?= $row['name']?></td><td><?= $row['continent']?></td><td><?= $row['independence_year']?><td><?= $row['head_of_state']?></td></td></tr>
+  
+  
 <?php endforeach; ?>
-</ul>
+</table>
